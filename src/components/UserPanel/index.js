461ab55dc9251785e6a685afd308e0eb48c68d95
Martin Aleksiev <chochinize@gmail.com>
2022-05-04 16:41:00 +0200

    branch b

--- src/components/UserPanel/index.js
+++ src/components/UserPanel/index.js
@@ -4,6 +4,8 @@ import Calories from "../Calories";
 import axios from "axios";
 import useAuth from "../hooks/useAuth";
 import Loaders from "../Loaders";
+
+import LineChart from "../Charts/LineChart";
 const index = () => {
   const id = useParams();
 
@@ -37,21 +39,23 @@ const index = () => {
       <div className=" px-4 xs:w-full grid gap-4   ">
         <h1 className="text-4xl font-extrabold">
           <div className="flex gap-2 ">
-            <div className="grow max-w-full  xs:invisible sm:invisible md:invisible  border-2">3</div>
+            <div className="grow max-w-full  xs:hidden sm:invisible lg:visible  ">3</div>
             <div className="grow max-w-[550px] ">
               <>
                 {" "}
-                <div className="h-8 xs:square sm:h-auto xs:square border-[1px] border-black mb-2 rounded-[10px] xs:h-auto md:square md:h-auto ">
+                <div className="h-8 xs:square sm:h-auto xs:square border-[2px] bg-gradient-to-r from-white to-white border-black mb-2 rounded-[10px] xs:h-auto md:square md:h-auto ">
                     
                   <Calories />
                   {/* <Loaders/> */}
                 </div>
-                <div className="h-8 xs:square sm:h-auto sm:square md-full:square xs:s border-[1px] border-black mb-2 rounded-[10px]  xs:h-auto md:h-auto ">Make your health your priority</div>
+                <div className="h-8 xs:square sm:h-auto sm:square md-full:square xs:s border-[1px] border-black mb-2 rounded-[10px]  xs:h-auto md:h-auto ">
+                    <LineChart/>
+                </div>
                 <div className="h-8 bg-blue-500 sm:square sm:h-auto xs:square xs:h-auto md:square md:h-auto  ">Here is Chart</div>
                 <div className="h-8 bg-blue-500 xs:square xs:h-auto md:square md:h-auto"></div>
               </>
             </div>
-            <div className="grow max-w-full  xs:invisible sm:invisible  lg:visible  border-2">2</div>
+            <div className="grow max-w-full  xs:hidden sm:invisible  lg:visible  border-2">2</div>
           </div>
         </h1>
       </div>}
