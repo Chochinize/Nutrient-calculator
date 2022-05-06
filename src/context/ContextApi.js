1bd8d6d78e0f4f7cb144e7bea4b897e34847d471
Martin Aleksiev <chochinize@gmail.com>
2022-05-07 01:26:37 +0200

    esc

--- src/context/ContextApi.js
+++ src/context/ContextApi.js
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState, useReducer } from "react";
 import { authenticateUser } from "../helpers/apiCalls";
 import reducer  from "./reducer";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const GlobalContext = createContext();
 
@@ -9,7 +10,7 @@ const ContextApi = ({ children }) => {
   const [user, setUser] = useState(null);
   const [authIsDone, setAuthIsDone] = useState(false);
   const [show, setShow] = useState(false);
-
+  const navigate = useNavigate();
   const initialState = { 
     notify: {}, auth: {}, cart: [], modal: [], orders: [], users: [], categories: [],products:[] 
 
@@ -26,6 +27,7 @@ useEffect(() => {
       dispatch({type:'PRODUCTS', payload:res})
     };
     fetchData();
+
   } catch (error) {
     return error;
   }
@@ -40,7 +42,9 @@ useEffect(() => {
         if (!res.error) {
           dispatch({ type: 'AUTH', payload:res})
           setUser(res);
+          console.log('fast data check',res._id)
           setAuthIsDone(true);
+          navigate(`/u/${res._id}`);
           return;
         }
       } catch (error) {}
