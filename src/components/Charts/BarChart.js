a9f2004dae77ac69520dbd9a1bc02d14d017f35b
Martin Aleksiev <chochinize@gmail.com>
2022-05-25 09:10:01 +0200

    charts

--- src/components/Charts/BarChart.js
+++ src/components/Charts/BarChart.js
@@ -1,5 +1,4 @@
 import React,{useState, useEffect} from 'react';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 import useAuth from '../hooks/useAuth';
 
